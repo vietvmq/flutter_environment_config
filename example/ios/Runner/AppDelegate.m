f06873c8ee11e381cf690317ae11d52b4dd4f0ee
vietvmq <vietvmq.dev@gmail.com>
2025-11-03 05:22:18 +0700

    update: .env project

--- example/ios/Runner/AppDelegate.m
+++ example/ios/Runner/AppDelegate.m
@@ -2,18 +2,17 @@
 #include "GeneratedPluginRegistrant.h"
 
 // import the plugin
-// #import "FlutterConfigPlugin.h"
+// #import "FlutterEnvironmentConfigPlugin.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    
     // then read individual keys like:
-     // NSString *apiUrl = [FlutterConfigPlugin envFor:@"API_URL"];
-  [GeneratedPluginRegistrant registerWithRegistry:self];
-  // Override point for customization after application launch.
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+    // NSString *apiUrl = [FlutterEnvironmentConfigPlugin envFor:@"API_URL"];
+    [GeneratedPluginRegistrant registerWithRegistry:self];
+    // Override point for customization after application launch.
+    return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
 @end

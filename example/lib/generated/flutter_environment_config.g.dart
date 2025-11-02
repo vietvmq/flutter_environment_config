23951b7d5fdc511d3115f03951e4eebbbc86c4ab
vietvmq <vietvmq.dev@gmail.com>
2025-11-02 13:11:54 +0700

    update: example

--- example/lib/generated/flutter_environment_config.g.dart
+++ example/lib/generated/flutter_environment_config.g.dart
@@ -1,5 +1,5 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
-// Generated on: 2025-11-02T13:04:47.205661
+// Generated on: 2025-11-02T13:11:26.991528
 // Variables: 6 from 3 environment files
 // Project: Consumer
 
